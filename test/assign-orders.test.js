21918e8a081073641f898e4d544d266991e61109
Thanasis Polychronakis <thanpolas@gmail.com>
2016-02-11 21:26:23 +0200

    scaffold testing for assign-orders

---
+++ test/assign-orders.test.js
@@ -0,0 +1,16 @@
+/**
+ * @fileOverview Test assign orders.
+ */
+var path = require('path');
+
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('Assign Orders test', function() {
+
+  describe('Nominal Operation', function () {
+    it('Should calculate as expected', function () {
+
+    });
+  });
+});
